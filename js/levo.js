c3dce27e7086392ab3d63b70893415856a989dda
Rahul Sethuram <rksethuram9@gmail.com>
2017-07-10 14:44:00 -0700

    Initial commit

---
+++ js/levo.js
@@ -0,0 +1,26 @@
+var database = firebase.database();
+
+function addEmailAddress() {
+  var emailAddress = $("#email-input").val();
+  console.log("Adding email address " + emailAddress);
+  var newEmailRef = database.ref("email").push(emailAddress);
+  console.log(newEmailRef.toString());
+}
+
+function buttonTextForScreenSize(windowWidth) {
+  if (windowWidth < 1200) {
+    $("#cta-button").html("Sign Up!");
+  } else {
+    $("#cta-button").html("Be the first to get the app!");
+  }
+}
+
+(function($) {
+  var windowWidth = $(window).width();
+  buttonTextForScreenSize(windowWidth);
+
+  $(window).resize(function() {
+    var windowWidth = $(window).width();
+    buttonTextForScreenSize(windowWidth);
+  });
+})(jQuery);
