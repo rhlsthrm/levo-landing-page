9b8094cc3afe509c9363dca9b86c15334c2c82df
Rahul Sethuram <rksethuram9@gmail.com>
2017-07-25 10:36:56 -0700

    Add files.

--- js/levo.js
+++ js/levo.js
@@ -1,11 +1,56 @@
 var database = firebase.database();
 
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 function addEmailAddress() {
-  var emailAddress = $("#email-input").val();
+  var emailAddress = $("#email-input").val().toLowerCase();
+  if (!emailAddress) {
+    $("#alert-text").html("Email address can't be blank.");
+    $("#alert").show();
+    return;
+  }
+  var re = /\S+@\S+\.\S+/;
+  if (!re.test(emailAddress)) {
+    $("#alert-text").html("Please use a valid email address.");
+    $("#alert").show();
+    return;
+  }
   console.log("Adding email address " + emailAddress);
-  var newEmailRef = database.ref("email").push(emailAddress);
-  console.log(newEmailRef.toString());
-  fbq("track", "Lead", {});
+  var source = getParameterByName("source");
+  source = source ? source : "Other";
+  var client = new ClientJS();
+  database
+    .ref("email")
+    .orderByChild("email")
+    .equalTo(emailAddress.toLowerCase())
+    .once("value")
+    .then(function(dataSnapshot) {
+      if (dataSnapshot.val()) {
+        $("#alert-text").html("Email address already registered.");
+        $("#alert").show();
+      } else {
+        var newEmailRef = database.ref("email").push({
+          email: emailAddress.toLowerCase(),
+          userAgent: client.getUserAgent(),
+          timestamp: Date.now(),
+          source: source
+        });
+        console.log(newEmailRef.toString());
+        $("#alert-text").html("Thank you for signing up!");
+        $("#alert").show();
+        fbq("track", "Lead", {});
+        goog_report_conversion("http://www.levo.finance");
+      }
+    });
+  return;
 }
 
 function buttonTextForScreenSize(windowWidth) {
@@ -17,6 +62,7 @@ function buttonTextForScreenSize(windowWidth) {
 }
 
 (function($) {
+  console.log(getParameterByName("source"));
   var windowWidth = $(window).width();
   buttonTextForScreenSize(windowWidth);
 
